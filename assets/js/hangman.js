06de4b25a0eda1653af4063de43b66de19f38e9d
Hnin Haymar <hninhaymar@gmail.com>
2018-02-15 00:18:19 -0800

    refactored using javascript object and grouped functions together

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,52 +1,52 @@
-
-//get the word's length, display the __ __ __ __ placeholders
-
-//get user's input - from onkeyup event 
-
-//make a word bank - an array of list of words
+//Global Variables - these won't be refreshed at every game reset
 var wordBank = ['name','role','family','mother','son','daughter','children'];
+var wins = 0; 
+var losses = 0;
+var wordBlanks = document.getElementById("word-blanks");
+
+//game object
+var hangmanGame = {
+    guessesLeft : 9,
+    wrongGuesses : "",
+    placeholder : "",
+    randomWord : "",
+    paddedRandomWord : "",
+    randomLength : 0,
+    resetGame : function(){
+        this.guessesLeft = 9;
+        this.wrongGuesses = "";
+
+        this.randomWord = generateRandomWord();
+        this.randomLength = this.randomWord.length;
+        console.log("length of " + this.randomWord + " is " + this.randomLength);
+        this.placeholder = "";
+        this.paddedRandomWord = "";
+
+        for(i = 0; i < this.randomLength; i++){
+            this.placeholder += "_ ";
+            this.paddedRandomWord += this.randomWord[i] + " ";
+        }
+        console.log(this.paddedRandomWord);
+        //set placeholder (gives users a hint of the length of the word)
+
+        wordBlanks.textContent = this.placeholder;
+        document.getElementById("guesses-left").textContent =  this.guessesLeft;
+        document.getElementById("wrong-guesses").textContent =  this.wrongGuesses;
+    }
+};
 
 function replaceAt(string, index, replace) {
     return string.substring(0, index) + replace + string.substring(index + 1);
 }
 
-var wins = 0; 
-var losses = 0;
-var guessesLeft = 9;
-var wrongGuesses = "";
-var placeholder = "";
-var randomWord;
-var paddedRandomWord = "";
-var wordBlanks = document.getElementById("word-blanks");
-var randomLength = 0;
-
-function resetGame(){
-    guessesLeft = 9;
-    wrongGuesses = "";
-    //randomly generate a word form the bank; i.e, "name"
-    randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    randomLength = randomWord.length;
+function generateRandomWord() {
+    var  randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     console.log("randomWord: " + randomWord);
-    console.log("length of " + randomWord + " is " + randomLength);
-    placeholder = "";
-    paddedRandomWord = "";
-
-    for(i = 0; i < randomLength; i++){
-        placeholder += "_ ";
-        paddedRandomWord += randomWord[i] + " ";
-    }
-    console.log(paddedRandomWord);
-    //set placeholder (gives users a hint of the length of the word)
-
-    wordBlanks.textContent = placeholder;
-    document.getElementById("guesses-left").textContent =  guessesLeft;
-    document.getElementById("wrong-guesses").textContent =  wrongGuesses;
+    return randomWord;
 }
+hangmanGame.resetGame();
 
 // Next, get user key input, using onkeyup event
-
-resetGame();
-
 document.onkeyup = function(event) {
     var userKey = event.key;
     console.log("User Entered: " + userKey);
@@ -55,45 +55,45 @@ document.onkeyup = function(event) {
 
     //decrement guessesLeft
     var wrong = false;
-    if(placeholder != paddedRandomWord && (guessesLeft > 0)){
-        guessesLeft--;
-        for(var i = 0; i < randomLength; i++){
-            if(randomWord.charAt(i) === userKey){
-                placeholder = replaceAt(placeholder, i*2, userKey);
+    if(hangmanGame.placeholder != hangmanGame.paddedRandomWord && (hangmanGame.guessesLeft >= 0)){
+        hangmanGame.guessesLeft--;
+        for(var i = 0; i < hangmanGame.randomLength; i++){
+            if(hangmanGame["randomWord"].charAt(i) === userKey){
+                hangmanGame.placeholder = replaceAt(hangmanGame.placeholder, i*2, userKey);
                 wrong = true;
             }
         }
         if(!wrong){
-            wrongGuesses+= userKey + " ";
+            hangmanGame.wrongGuesses+= userKey + " ";
         }
 
-        if(placeholder === paddedRandomWord){
+        if(hangmanGame.placeholder === hangmanGame.paddedRandomWord){
             wins++;
-            wordBlanks.textContent = placeholder;
-            alert("Yay!! You Win!");
-            var reset = confirm("Do you want to reset the game?");
-            if(reset){
-                resetGame();
-            }
+            console.log("Won! here's the placeholder now. " + hangmanGame.placeholder);
+            wordBlanks.textContent = hangmanGame.placeholder;
+            alert("Yay!! You Win! Successfully guessed the word : " + hangmanGame.randomWord);
+            hangmanGame.resetGame();
             
         }
-        else if(guessesLeft <= 0 && (placeholder != paddedRandomWord)){
+        else if(hangmanGame.guessesLeft < 0 && (hangmanGame.placeholder != hangmanGame.paddedRandomWord)){
             alert("Lost this round! Try again");
             losses++;
-            resetGame();
+            hangmanGame.resetGame();
         }
+
+        
     }
 
-    wordBlanks.textContent = placeholder;
-    document.getElementById("guesses-left").textContent =  guessesLeft;
-    document.getElementById("wrong-guesses").textContent =  wrongGuesses;
+    wordBlanks.textContent = hangmanGame.placeholder;
+    document.getElementById("guesses-left").textContent =  hangmanGame.guessesLeft;
+    document.getElementById("wrong-guesses").textContent =  hangmanGame.wrongGuesses;
     document.getElementById("loss-counter").textContent =  losses;
     document.getElementById("win-counter").textContent =  wins;
+
+    
 };
 
 
+    
 
-// (h*30) + (m/12) - (m*6)
-// 4:15 ==> (4*30) + (15/12) - (15*6) = 120 + 1.25 -90 = 31.25
-// 6:41 == > (6*30) + (41/12) - (41*6) = 180 + 3.41 - 246 = 62.59
 
